cf335f37c4ee0007c47f55e81f304a3ddadf8c70
Sonderhen <silentfandub@gmail.com>
2024-01-28 19:06:37 -0300

    Update checksum.js

--- scripts/checksum.js
+++ scripts/checksum.js
@@ -5,6 +5,7 @@ var checksumsPermitidos = {
   "3d4f2bf07dc1be38b20cd6e46949a107": true,
   "8f97850eae897ee5371bb9527786aee771178ef9d2cce6c0e2ef62ab0e1463b3": true,
   "3b61b354c46ebc0244b49106a0a70fa399ff87592e49a14d4d7309fca21577a0": true,
+  "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855": true,
   // Adicione mais checksums conforme necessário
 };
 
@@ -19,39 +20,31 @@ function verificarChecksum(checksum) {
 }
 
 function fazerDownload(nomeDoArquivo) {
-  // Defina a URL do diretório desejado antes de usá-la
   var diretorioDesejado = "/files/retrodub/";
-
-  // Construa o caminho completo do arquivo
   var caminhoDoArquivo = diretorioDesejado + nomeDoArquivo;
 
   fetch(caminhoDoArquivo)
-      .then(response => response.arrayBuffer())
-      .then(buffer => {
-          var conteudoDoArquivo = new Uint8Array(buffer);
-          var checksum = calcularChecksumDoArquivo(conteudoDoArquivo);
+      .then(response => response.blob())
+      .then(blob => {
+          var checksum = calcularChecksumDoArquivo(new Uint8Array(blob));
           console.log(`Checksum do arquivo ${nomeDoArquivo}: ${checksum}`);
 
           if (verificarChecksum(checksum)) {
-              // O checksum está na lista permitida, permitir o download
-              var diretorioDesejado = "/files/retrodub/";
-              var caminhoDoArquivo = diretorioDesejado + nomeDoArquivo;
-          
-              // Cria um link temporário
+              // O checksum está na lista permitida, preparar o download
               var link = document.createElement('a');
-              link.href = caminhoDoArquivo;
+              link.href = URL.createObjectURL(blob);
               link.target = '_blank';
               link.download = nomeDoArquivo;
-          
+
               // Adiciona o link ao documento
               document.body.appendChild(link);
-          
+
               // Simula um clique no link para iniciar o download
               link.click();
-          
+
               // Remove o link do documento
               document.body.removeChild(link);
-              window.URL.revokeObjectURL(url);
+              window.URL.revokeObjectURL(link.href);
           } else {
               // O checksum não está na lista permitida, recusar o download
               console.error("O checksum do arquivo não é válido. O download foi recusado.");
